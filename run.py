d4435c2a4d269dffa1732f0a15830cf8c552ac2a
Brindhalakshmi Subramani <85474114+Brindhalakshmi17@users.noreply.github.com>
2024-06-28 07:04:22 +0530

    run app changes

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
 from app import create_app
 
-app = create_app('development')
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
